9a5d124d5b5bcbb7c325264dc1de96dae8f5750f
ionpostolache <ion_postolache00@yahoo.com>
2021-05-22 21:21:22 +0300

    complete, working but it may be too long

--- calculate_tip.py
+++ calculate_tip.py
@@ -18,11 +18,23 @@ try:
     bill_float=float(bill)
 except:
     print("Please enter the bill value as a number!")
-print(f"The bill value is: {bill_float}")
+#print(f"The bill value is: {bill_float}")
 
-tip18=bill_float*0.18
-#tip20=
-#tip25=
+def aroundup(a):
+    if a==int(a):
+        return a
+    else:
+        return int(a)+1
 
-print(f"18% tip is {tip18}, which brings your total to {bill_float+tip18}")
+#the following 3 lines will show a float sometimes(when there is nothing to roundup)
+print(f"18% tip is {bill_float*0.18}, which brings your total to {int(aroundup((bill_float*0.18)+bill_float))}.")
+print(f"20% tip is {bill_float*0.20}, which brings your total to {int(aroundup((bill_float*0.20)+bill_float))}.")
+print(f"25% tip is {bill_float*0.25}, which brings your total to {int(aroundup((bill_float*0.25)+bill_float))}.")
 
+no_people=input("How many people are involved? \n>")
+no_people=int(no_people)
+print("If splitted evenly:")
+print(f"18% tip is {bill_float*0.18/no_people}/each, which brings your total to {int(aroundup((bill_float+(bill_float*0.18))/no_people))}/each.")
+print("If spliteed 30% and 70%:")
+print(f"18% tip for the 1st person is {bill_float*0.18*0.3}, which brings your total to {aroundup((bill_float+(0.18*bill_float))*0.3)}.")
+print(f"18% tip for the 2nd person is {bill_float*0.18*0.7}, which brings your total to {aroundup((bill_float+(0.18*bill_float))*0.7)}.")
