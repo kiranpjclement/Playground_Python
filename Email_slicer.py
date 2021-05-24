200e72fb1dbf869aa991c0a57324f54f9dd29519
ionpostolache <ion_postolache00@yahoo.com>
2021-05-24 15:00:32 +0300

    working and finished.

--- Email_slicer.py
+++ Email_slicer.py
@@ -7,22 +7,21 @@ Input: peter.pan@myfantasy.com
 Output: Hey Peter, looks like you've got your own custom setup at MyFantasy. Impressive!.
 This is a convenient python project that has a lot of use in the future. The program helps
 get you the username and domain name from an email address.
-If you want to push this further, you can customize the application and send
-a message to the host with this information.
 """
 #I'm trying to take the email provider from a dictionary containing providers and domains
-email_providers=[]
-email_domains=[]
+email_providers=["Outlook", "Apple", "AOL Mail", "ProtonMail", "Google", "Yahoo", "Hotmail"]
+email_domains=["@outlook.com", "@icloud.com", "@aol.com", "@protonmail.com", "@gmail.com", "@yahoo.com", "@hotmail"]
+dictio=dict(zip(email_providers, email_domains))
+#print(dictio)
 
 email=input("Please type your email address > ")
-#print(email)
 
 #first I need to find the index for '@'
 #then slice the string by that index
 atindex=email.find('@')
 #print(atindex)
-domain=email[atindex+1:]
-#print(domain)
+domain=email[atindex:]
+#print(f"The domain is {domain}")
 username=email[:atindex]
 #print(username)
 #now I need to try to split a first name from the username
@@ -33,7 +32,16 @@ elif "." in username:
 
 if fname:
     first_name = username[:fname]
-    print(first_name)
+    first_namet=first_name.title()
+    #print(first_name)
 
-print(f"Hey {first_name}, looks like your email is registered with {domain}. That's cool!")
-print(f"Hey {first_name}, looks like you've got your own custom setup at {domain}. Impressive!")
+for k, v in dictio.items():
+    if v==domain:
+        dom=k
+        #print(k)
+
+if domain in email_domains:
+    print(f"Hey {first_namet}, looks like your email is registered with {dom}. That's cool!")
+else:
+    print(f"Hey {first_namet}, looks like you've got your own custom setup at {email[atindex+1:].title()}. Impressive!")
+
