32cc733682f2f02ae561d514ef14c1ff009eda79
ionpostolache <ion_postolache00@yahoo.com>
2021-05-27 22:00:19 +0300

    developed a dedicated code only for this exercise

--- check_perfect_square.py
+++ check_perfect_square.py
@@ -1,3 +1,22 @@
 """4. Write a Python program to check if a number is a perfect square.
 Input : 9, Output : True
 """
+
+thenumber=int(input("What number you want to check? > "))
+
+def square(n):
+    #n=0
+    while n<thenumber:
+        n=n+1
+        x=n*n
+        #print(x)
+        if x==thenumber:
+            #print(x==thenumber)
+            break
+        else:
+            #print(x==thenumber)
+            continue
+
+    print(x == thenumber)
+
+square(0)
