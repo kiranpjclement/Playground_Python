a9ca4c0ebe0f675365553ab41419cb4a51b66b76
ionpostolache <ion_postolache00@yahoo.com>
2021-05-28 15:40:33 +0300

    working

---
+++ square_root.py
@@ -0,0 +1,9 @@
+"""11. Write a Python program to compute and return the square root of a given 'integer'.
+Input : 16
+Output : 4
+Note : The returned value will be an 'integer'   """
+
+inputno=int(input("What number you want to compute square root for: > "))
+output=inputno**(1/2)
+outputint=int(output)
+print(outputint)
