a35eb50056d755d8121c39f1f913552c6c75612b
Kiran Clement <74559018+kiranpjclement@users.noreply.github.com>
2021-05-28 17:08:50 +0100

    Update no_occur_twice.py

--- no_occur_twice.py
+++ no_occur_twice.py
@@ -2,10 +2,9 @@
 Input : [5, 3, 4, 3, 4]
 Output : 5   """
 
-
 list1=[5, 3, 4, 3, 4]
 list2=list1
-ouputlist=[]
+outputlist=[]
 
 for i in list1:
     k=0
@@ -13,5 +12,5 @@ for i in list1:
         if i ==j:
             k=k+1
     if k<2:
-        oplist.append(i)
-print(oplist)
+        outputlist.append(i)
+print(outputlist)
