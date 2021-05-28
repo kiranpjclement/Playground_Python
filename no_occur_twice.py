ec87645730a0a2f3b5eee1a85b8f0dd071c6f04d
Kiran Clement <74559018+kiranpjclement@users.noreply.github.com>
2021-05-28 16:59:19 +0100

    Using nested for loop

--- no_occur_twice.py
+++ no_occur_twice.py
@@ -3,48 +3,15 @@ Input : [5, 3, 4, 3, 4]
 Output : 5   """
 
 
-inputlist=[5, 3, 4, 3, 4]
-print(f"The input list is: {inputlist}")
-resultlist=[]
-"""
-for i in inputlist:
-    #print(i)
-    indx=inputlist.index(i)
-    print(id(inputlist[indx]))
-    popul=inputlist.pop(indx)
-    print(popul)
-    #print(indx)
-    #bbb=inputlist[indx]
-    #print(bbb)
-    #popul=inputlist.pop(indx)
-    #print(popul)
-    #bbb=inputlist[indx]
-    #print(bbb)
-    resultlist.append(popul)
-    #print(i)
-    #i=i+1
+list1=[1,3,3,5,5,6]
+list2=list1
+ouputlist=[]
 
-print(resultlist)
-print(inputlist)
-
-for j in inputlist:
-    inputlist.pop()
-    print(inputlist)
-
-print(inputlist)
-"""
-for i in range(0,len(inputlist)):
-    #print(i)
-    each_no=inputlist[i]
-    print(each_no)
-    current_no=inputlist.pop(i)
-    print(current_no)
-    print(inputlist)
-    if current_no in inputlist:
-        print(f"{current_no} is twice")
-        continue
-    else:
-        resultlist.append(current_no)
-
-print(inputlist)
-print(f" these numbers are only once: {resultlist}")
+for i in list1:
+    k=0
+    for j in list2:
+        if i ==j:
+            k=k+1
+    if k<2:
+        oplist.append(i)
+print(oplist)
